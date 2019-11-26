b3b167a94a9660ce36e39bf1cc2e40ef240cadab
JenniKate Wallace <jennikate@gmail.com>
2019-11-26 11:23:21 +0000

    files

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import './style.scss'
+
+import Home from './components/Home'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home} />
+    </Switch>
+  </BrowserRouter>
+
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
