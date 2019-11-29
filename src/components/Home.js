5e782bce80ae8fbdde05f367fd1a7ab69eb7fdf8
JenniKate Wallace <jennikate@gmail.com>
2019-11-29 12:15:37 +0000

    refactored to hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,53 +1,80 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-class Home extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      monsterList: [],
-      singleMonster: ''
-    }
-  }
 
-  componentDidMount() {
-    this.getMonsters()
-  }
+const Home = () => {
 
+  const [monsters, setMonsters] = useState([])
+  useEffect(() => {
+    axios.get('https://api.open5e.com/monsters/?limit=100')
+      .then(resp => setMonsters(resp.data.results))
+      .catch(err => console.log(err))
+  })
 
-  getMonsters() {
-    axios.get('https://api.open5e.com/monsters/')
-      // .then(resp => console.log(resp.data.results))
-      .then(resp => this.setState({ monsterList: resp.data.results }))
-      .catch(err => this.setState({ errors: err.response.data.error }))
-      .then(console.log('gottem'))
-  }
-
-
-
-
-
-  render() {
-    if (!this.state.monsterList.length === 0) {
-      return <h1>Loading Monsters</h1>
-    }
-    console.log(this.state.monsterList)
-    return (
-      <div>
-        <p>Hi</p>
-
-        {this.state.monsterList.map((elem, i) => {
-          return (
-            <div key={i}>
-              <p>{elem.name}</p>
-            </div>
-          )
-        })}
-
-      </div>
-    )
-  }
-
+  return (
+    <div>
+      <h1>Monster List</h1>
+      {monsters.map((monster, i) => {
+        return (
+          <p key={i}>{monster.name}</p>
+        )
+      })}
+    </div>
+  )
 }
+
+
+
+//========
+// Original, using class
+//========
+// class Home extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       monsterList: [],
+//       singleMonster: ''
+//     }
+//   }
+
+//   componentDidMount() {
+//     this.getMonsters()
+//   }
+
+
+//   getMonsters() {
+//     axios.get('https://api.open5e.com/monsters/')
+//       // .then(resp => console.log(resp.data.results))
+//       .then(resp => this.setState({ monsterList: resp.data.results }))
+//       .catch(err => this.setState({ errors: err.response.data.error }))
+//       .then(console.log('gottem'))
+//   }
+
+
+
+
+
+//   render() {
+//     if (!this.state.monsterList.length === 0) {
+//       return <h1>Loading Monsters</h1>
+//     }
+//     console.log(this.state.monsterList)
+//     return (
+//       <div>
+//         <p>Hi</p>
+
+//         {this.state.monsterList.map((elem, i) => {
+//           return (
+//             <div key={i}>
+//               <p>{elem.name}</p>
+//             </div>
+//           )
+//         })}
+
+//       </div>
+//     )
+//   }
+
+// }
 
 export default Home
