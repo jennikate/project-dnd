221bb0ccd8bb186a85ecbfcfb33f8bf810f91982
JenniKate Wallace <jennikate@gmail.com>
2019-12-15 15:11:48 +0000

    mapped speeds to ui

--- src/components/SingleMonster.js
+++ src/components/SingleMonster.js
@@ -1,22 +1,52 @@
 /* eslint-disable brace-style */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useFetch from '../hooks/useFetch'
 
 
 
 
 const SingleMonster = (props) => {
+
   //get the single monster data
-  const slug = props.match.params.slug
-  const monster = useFetch(`https://api.open5e.com/monsters/${slug}`)
+  const monster = useFetch(`https://api.open5e.com/monsters/${props.match.params.slug}`)
 
-  //
 
-  // console.log(typeof monster.speed)
-  // if (!monster.speed) { console.log('loading') } else {
-  //   console.log(Object.keys(monster.speed))
-  //   console.log(Object.values(monster.speed))
-  // }
+  //turn the objects we need into arrays for us to map :: can probably extract some of these out to function/calculations later
+  const [speed, setSpeed] = useState([])
+  const [saves, setSaves] = useState([])
+
+  // //create array from monster objects and speed objects so can map them
+  function getSpeed() {
+    const speedArr = []
+    if (monster.length === 0) {
+      console.log('waiting on data')
+    }
+    else {
+      for (const [key, value] of Object.entries(monster.speed)) {
+        speedArr.push({ type: key, speed: value })
+      }
+      setSpeed(speedArr)
+    }
+  }
+
+  //create array of save stats
+  function getSaves() {
+    if (monster.length === 0) {
+      console.log('saves - waiting on data')
+    }
+    else {
+      console.log('saves got data')
+      console.log(monster)
+
+     //THIS NEEDS TO GET OBJECTS WITH _save AND TURN INTO AN ARRAY SO CAN MAP TO UI
+    }
+  }
+
+
+
+
+
+
 
   //calculate the monster stat modifier (can probably extract this to calculations later)
   //round down (stat - 10 ) / 2
@@ -25,9 +55,14 @@ const SingleMonster = (props) => {
   }
 
 
+
+
+
+
+
+
   // //Find the saves
   function saveStats() {
-
     //I think I should be able to map or filter this, but monster is an object and I ran into trouble so using for atm
     for (var key in monster) {
       if (key.endsWith('_save') && monster[key] !== null) {
@@ -38,8 +73,17 @@ const SingleMonster = (props) => {
     }
   }
 
+
+  //run the functions when the monster data changes
+  useEffect(() => {
+    getSpeed()
+    getSaves()
+  }, [monster])
+
+
+
   //loading
-  if (monster.data === 0) {
+  if (monster.length === 0) {
     return <h1>Loading</h1>
   }
   return (
@@ -57,11 +101,11 @@ const SingleMonster = (props) => {
           <p className='title'>Hit Points</p>
           <p>{monster.hit_points} ({monster.hit_dice})</p>
           <p className='title'>Speed</p>
-          {/* {Object.entries(monster.speed).map(([key, value]) => {
+          {speed.map((elem, i) => {
             return (
-              <p key={key}>{key}, {value}</p>
+              <p key={i}>{elem.type}: {elem.speed}</p>
             )
-          })} */}
+          })}
           <div className='core-stats'>
             <ul>
               <li>
